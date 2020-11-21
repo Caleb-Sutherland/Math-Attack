f20c1a2f6711a41e7e8f84b6bbef8ce611fbfa9b
Jack <jahill@live.ca>
2020-11-21 18:32:25 -0500

    database rework

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, g
 
 import sqlite3
 from random import random
@@ -6,16 +6,19 @@ from random import random
 
 app = Flask(__name__)
 
-#create database and user table within
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
-print("Opened database successfully")
+DATABASE = 'database.db'
 
-result = cursor.execute("SELECT name from sqlite_master WHERE type='table' AND name='users'")
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
-if(int(len(result.fetchall())) == 0):
-	cursor.execute('CREATE TABLE users (id INTEGER NOT NULL, name TEXT NOT NULL, health INTEGER NOT NULL, PRIMARY KEY(id))')
-cursor.close()
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/")
 def home():
@@ -23,6 +26,12 @@ def home():
 	return render_template("create_user.html")
 
   #return render_template("attack.html", question = "1 + 2")
+    
+@app.route("/check/")
+def check():
+    result = get_db().execute("SELECT name FROM users")
+    value = str(result.fetchall())
+    return value
 
 if __name__ == "__main__":
 	app.run(debug = True)
