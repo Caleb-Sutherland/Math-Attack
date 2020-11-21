d4de9f3066f72ceaf433023cb4a01ae72671fe17
caleb <csuthe42@uwo.ca>
2020-11-21 15:15:00 -0500

    add database code

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+import sqlite3
 
 app = Flask(__name__)
+
+#create database and user table within
+conn = sqlite3.connect('database.db')
+print("Opened database successfully")
+conn.execute('CREATE TABLE users (name TEXT, health INTEGER)')
+print("Table created successfully")
+conn.close()
 
 @app.route("/")
 def home():
