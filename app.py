5a7ccb7200a02aff4e0b964f9fa5e3f581b2c234
Jack <jahill@live.ca>
2020-11-21 15:57:03 -0500

    stylized attack page

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from random import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("attack.html")
+    return render_template("attack.html", question = "1 + 2")
 
 if __name__ == "__main__":
 	app.run(debug = True)
